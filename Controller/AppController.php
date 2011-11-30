03d5a328fe00539009a74ebacb6ae4c1cf04ca8f
Andras Kende <andras@kende.com>
2011-11-30 14:39:19 -0600

    changes

--- Controller/AppController.php
+++ Controller/AppController.php
@@ -3,36 +3,36 @@ App::uses('Controller', 'Controller');
 class AppController extends Controller {
 
 	public $components = array(
-		'Session', 
+		'Session',
 		'DebugKit.Toolbar',
 		'Auth'
 	);
 
 	public function beforeFilter() {
-		
-        $this->Auth->loginAction = array('controller' => 'users', 'action' => 'login', 'admin' => false);
-        $this->Auth->loginRedirect = array('controller' => 'orders', 'action' => 'index', 'admin' => true);
-        $this->Auth->logoutRedirect = array('controller' => 'products', 'action' => 'index', 'admin' => false);
 
-        $this->Auth->authenticate = array(
-            AuthComponent::ALL => array(
-                'userModel' => 'User',
-                'fields' => array(
-                    'username' => 'username',
-                    'password' => 'password'
+		$this->Auth->loginAction = array('controller' => 'users', 'action' => 'login', 'admin' => false);
+		$this->Auth->loginRedirect = array('controller' => 'orders', 'action' => 'index', 'admin' => true);
+		$this->Auth->logoutRedirect = array('controller' => 'products', 'action' => 'index', 'admin' => false);
+
+		$this->Auth->authenticate = array(
+			AuthComponent::ALL => array(
+				'userModel' => 'User',
+				'fields' => array(
+					'username' => 'username',
+					'password' => 'password'
 				),
-	        	'scope' => array(
-	        		'User.is_active' => '3123'
-	        	)
-            ), 'Form'
-        );
+				'scope' => array(
+					'User.is_active' => '1'
+				)
+			), 'Form'
+		);
 
 		if(isset($this->request->params['admin']) && ($this->request->params['prefix'] == 'admin')) {
 			$this->layout = 'admin';
 		} else {
 			$this->Auth->allow('*');
 		}
-		
+
 	}
 
 }
