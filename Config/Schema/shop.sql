7339fb629a99b064ff67c00ed8bfdfc85eb271b6
Andras Kende <andras@kende.com>
2011-11-30 14:26:07 -0600

    changes

---
+++ Config/Schema/shop.sql
@@ -0,0 +1,121 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.7.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 30, 2011 at 02:24 PM
+-- Server version: 5.5.15
+-- PHP Version: 5.3.6
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `shop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `subtotal` decimal(8,2) NOT NULL,
+  `tax` decimal(8,2) NOT NULL,
+  `shipping` decimal(8,2) NOT NULL,
+  `total` decimal(8,2) NOT NULL,
+  `status` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_items`
+--
+
+CREATE TABLE IF NOT EXISTS `order_items` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `order_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `price` decimal(8,2) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `slug` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `image` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `price` decimal(8,2) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`),
+  KEY `modified` (`modified`),
+  KEY `name_slug` (`slug`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ROW_FORMAT=COMPACT AUTO_INCREMENT=11 ;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `slug`, `image`, `price`, `created`, `modified`) VALUES
+(1, 'Product 1', 'product1', '1.jpg', '9.95', '2011-10-12 04:04:08', '2011-11-28 06:32:03'),
+(2, 'Product 2', 'product2', '2.jpg', '19.95', '2011-10-12 04:04:08', '2011-11-28 15:49:29'),
+(3, 'Product 3', 'product3', '3.jpg', '19.95', '2011-10-12 04:04:10', '2011-11-28 00:50:07'),
+(4, 'Product 4', 'product4', '4.jpg', '19.95', '2011-10-12 04:04:10', '2011-11-27 13:08:33'),
+(5, 'Product 5', 'product5', '5.jpg', '19.95', '2011-10-12 04:04:10', '2011-11-28 15:43:53'),
+(6, 'Product 6', 'product6', '6.jpg', '49.95', '2011-10-12 04:04:10', '2011-11-27 04:19:02'),
+(7, 'Product 7', 'product7', '7.jpg', '19.95', '2011-10-12 04:04:10', '2011-11-27 09:43:42'),
+(8, 'Product 8', 'product8', '8.jpg', '79.95', '2011-10-12 04:04:10', '2011-11-27 12:05:26'),
+(9, 'Product 9', 'product9', '9.jpg', '19.95', '2011-10-12 04:04:10', '2011-11-28 00:50:03'),
+(10, 'Product 10', 'product10', '10.jpg', '99.99', '2011-10-12 04:04:10', '2011-11-27 07:43:26');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` char(36) COLLATE utf8_unicode_ci NOT NULL,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `is_active` tinyint(1) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ROW_FORMAT=COMPACT;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `is_active`, `created`, `modified`) VALUES
+('4e800ecf-b5bc-4fc6-b78f-67884317134f', 'a', '6cfb5e7ba5fa202e923f45c534b87344440591e9', 1, '2011-09-26 00:34:07', '2011-09-26 00:34:07');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
